0ce3248e4c207449666b0543bc29af25040880cb
Lukas Bo <107140455+LukasBo007@users.noreply.github.com>
2023-10-03 23:27:18 +0000

    Testoverlay

--- Das Spiel/Main.py
+++ Das Spiel/Main.py
@@ -6,3 +6,25 @@
 import random
 import pygame
  
+#Initialisierung
+pygame.init()
+
+#Screen
+screen = pygame.display.set_mode([400, 800])
+
+# Run 
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((43, 51, 64))
+
+    pygame.draw.circle(screen, (204, 188, 122), (200, 400), 100)
+
+    pygame.display.flip()
+
+#Quit
+pygame.quit()
