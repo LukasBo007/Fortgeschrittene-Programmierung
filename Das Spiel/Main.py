b40b195026e4053e4edf6b01f7dfb5ce5df6e724
Lukas Bo <107140455+LukasBo007@users.noreply.github.com>
2023-10-03 22:51:04 +0000

    aufräumen

---
+++ Das Spiel/Main.py
@@ -0,0 +1,20 @@
+#########################################################################
+#                         Shoot the Moon 
+#########################################################################
+
+#Importe
+import pygame
+
+#Initialisierung
+pygame.init()           
+pygame.mixer.init()    
+pygame.display.set_caption("Shoot the not Moon")   
+
+#Screen
+
+
+
+
+
+
+
